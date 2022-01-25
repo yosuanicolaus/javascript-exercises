d82f5381e8b65981e2f9824b3063e90d17d13e64
Yosua Nicolaus <cryodawn257@gmail.com>
2022-01-25 08:14:20 +0700

    finished 04, learned about 'arguments' obj

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for (let u=1; u<arguments.length; u++){
+        for (let i=0; i<array.length; i++){
+            if (array[i] === arguments[u]){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
