2071f55c2c1674c175306951c745f8c3ad135b61
Yosua Nicolaus <cryodawn257@gmail.com>
2022-01-25 08:30:14 +0700

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if (start < 0 || end < 0 || typeof start !== `number` || typeof end !== `number`) return `ERROR`;
+
+    let sum = 0;
+
+    if (start > end){
+        // swap the variable so that start is always lower
+        end = [start, start = end][0];
+    }
+
+    for (start; start <= end; start++){
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
