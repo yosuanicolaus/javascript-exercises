e9c89a4d679506bd714607954faa0ad2688717f4
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-06 06:00:50 +0700

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {
+    return 1;
+  } else {
+    isNegative = num < 0;
+    num = Math.abs(num);
+    let result = num;
+    while (num > 1) {
+      result *= (num - 1);
+      num--;
+    }
+    
+    if (isNegative) result *= -1;
+    return result;
+  }
+  
 };
 
 // Do not edit below this line
