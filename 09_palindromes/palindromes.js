5a55698690915c8cccfad757ec85ccd2e616aa3a
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-06 06:21:39 +0700

    finished 09, palindrome ez...

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const filtered = str.replace(/[\W_]/g, '');
+    const final = filtered.toLowerCase();
 
+    let i = 0;
+    let j = final.length - 1;
+
+    while (i < j) {
+        if (final[i] == final[j]) {
+            i++;
+            j--;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
