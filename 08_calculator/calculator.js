961155a0006ee5f778e718cfb4eb4cf83b086903
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-06 06:06:31 +0700

    convert to arrow function for shorter code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,22 +1,12 @@
-const add = function(a, b) {
-	return a + b;
-};
+const add = (a, b) => a + b;
 
-const subtract = function(a, b) {
-	return a - b;
-};
+const subtract = (a, b) => a - b;
 
-const sum = function(array) {
-  return array.reduce((a, b) => a + b, 0);
-};
+const sum = (array) => array.reduce((a, b) => a + b, 0);
 
-const multiply = function(array) {
-  return array.reduce((a, b) => a * b);
-};
+const multiply = (array) => array.reduce((a, b) => a * b);
 
-const power = function(a, b) {
-	return a ** b;
-};
+const power = (a, b) => a ** b;
 
 const factorial = function(num) {
 	if (num == 0) {
