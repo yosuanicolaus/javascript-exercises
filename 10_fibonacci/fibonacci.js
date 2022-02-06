c0701318111365234f5abfdb8ae68719b68cafcd
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-06 07:16:35 +0700

    finished 10 (was quite stuck here), update npm

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {return "OOPS"}
 
+    let fibArr = [0, 1];
+    for (let i = 2; i <= +num; i++) {
+        fibArr[i] = fibArr[i - 2] + fibArr[i - 1];
+    }
+    return fibArr[num];
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
