010c7c085aecd05186993f218094c7aa9ce76d86
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-07 17:20:10 +0700

    finished 12, I now understand reduce & map

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const age = people.map(x => {
+    if (x.yearOfDeath == undefined) {
+      x.yearOfDeath = new Date().getFullYear();
+    }
+    return x.yearOfDeath - x.yearOfBirth;
+  });
 
-};
+  const oldestValue = age.reduce((acc, cur) => {
+    return Math.max(acc, cur);
+  });
+
+  const oldestIndex = age.indexOf(oldestValue);
+
+  return people[oldestIndex];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
